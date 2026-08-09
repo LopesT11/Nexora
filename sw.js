4f7fb3c3fc2287608af03be63a7c90e65cfc9759
LopesT11 <lopestomas00@gmail.com>
2026-08-09 19:32:58 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='dealers-v26-base-anterior-20260809';
-const ASSETS=['./','./index.html','./style.css?v=26.0.0','./app.js?v=26.0.0','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='dealers-v27-base-anterior-20260809';
+const ASSETS=['./','./index.html','./style.css?v=27.0.0','./app.js?v=27.0.0','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{if(r&&r.ok){const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x))}return r}).catch(()=>caches.match(e.request,{ignoreSearch:true}).then(r=>r||caches.match('./index.html'))))});
